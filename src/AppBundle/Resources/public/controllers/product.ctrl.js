328f2ed2807d6d533333229ef366bf8958937bcb
Andrzej Piszczek <andrzej.piszczek@bluepaprica.com>
2016-01-17 18:03:22 +0100

    Add save file formating

--- src/AppBundle/Resources/public/controllers/product.ctrl.js
+++ src/AppBundle/Resources/public/controllers/product.ctrl.js
@@ -10,6 +10,8 @@ function ProductController ($scope, $http, $rootScope, Console) {
         product,
         similars;
 
+    $scope.saveFormat = 'JSON';
+
     $rootScope.$on('productSelected', function(event, model) {
         //$scope.setProductModel(model.id);
         $scope.product = model;
@@ -140,6 +142,45 @@ function ProductController ($scope, $http, $rootScope, Console) {
         });
     };
 
+    function json2xml(o, tab) {
+        var toXml = function(v, name, ind) {
+            var xml = "";
+            if (v instanceof Array) {
+                for (var i=0, n=v.length; i<n; i++)
+                    xml += ind + toXml(v[i], name, ind+"\t") + "\n";
+            }
+            else if (typeof(v) == "object") {
+                var hasChild = false;
+                xml += ind + "<" + name;
+                for (var m in v) {
+                    if (m.charAt(0) == "@")
+                        xml += " " + m.substr(1) + "=\"" + v[m].toString() + "\"";
+                    else
+                        hasChild = true;
+                }
+                xml += hasChild ? ">" : "/>";
+                if (hasChild) {
+                    for (var m in v) {
+                        if (m == "#text")
+                            xml += v[m];
+                        else if (m == "#cdata")
+                            xml += "<![CDATA[" + v[m] + "]]>";
+                        else if (m.charAt(0) != "@")
+                            xml += toXml(v[m], m, ind+"\t");
+                    }
+                    xml += (xml.charAt(xml.length-1)=="\n"?ind:"") + "</" + name + ">";
+                }
+            }
+            else {
+                xml += ind + "<" + name + ">" + v.toString() +  "</" + name + ">";
+            }
+            return xml;
+        }, xml="";
+        for (var m in o)
+            xml += toXml(o[m], m, "");
+        return tab ? xml.replace(/\t/g, tab) : xml.replace(/\t|\n/g, "");
+    }
+
     $scope.saveToPc = function (data, filename) {
 
         if (!data) {
@@ -154,8 +195,8 @@ function ProductController ($scope, $http, $rootScope, Console) {
         if (typeof data === 'object') {
             data = JSON.stringify(data, undefined, 2);
         }
-
-        var blob = new Blob([data], {type: 'text/json'}),
+        var displayData = ($scope.saveFormat=='JSON')?data:json2xml($scope.product);
+        var blob = new Blob([displayData], {type: 'text/'+$scope.saveFormat.toLowerCase()}),
             e = document.createEvent('MouseEvents'),
             a = document.createElement('a');
 
